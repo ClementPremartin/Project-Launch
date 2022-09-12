38ec06a997234bca3d1f90f4ef4cb0780cd70d99
ClementPremartin <clementpremartin@gmail.com>
2022-09-12 16:49:05 +0200

    setup sqlite

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const typeorm = require("typeorm");
 
 const app = express();
+
+const dataSource = new typeorm.DataSource({
+    type: "sqlite",
+    database: "wilersdb.sqlite",
+    synchronyse: true,
+});
 
 app.get("/", (req, res) => {
     res.send("App running on express server!");
 });
 
-app.listen(4000, () => console.log("Server Started On 4000"));
+const PORT = 4000;
+
+const start = async() => {
+    await dataSource.initialize();
+    app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`));
+}
+
+start();
