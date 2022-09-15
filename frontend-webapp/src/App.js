2821ab28b5b0914ed17d8b9e44424e922f1e3d2f
ClementPremartin <clementpremartin@gmail.com>
2022-09-15 11:58:09 +0200

    create Home page and refacto with styled components

--- frontend-webapp/src/App.js
+++ frontend-webapp/src/App.js
@@ -1,41 +1,25 @@
-import { useState } from "react";
-import { Container, Footer, Header, MainContainer, PageTitle} from "./App.styled";
+import { Container, Footer, Header, MainContainer} from "./App.styled";
 import { Paragraph } from "./styles/base-styles";
 
-import Wilder from "./components/Wilder";
-import wilders from "./data/wilders";
+import Home from "./pages/Home";
 
 function App() {
-
-  const [wildersStudent] = useState(wilders);
-
   return (
-    <div>
+    <>
       <Header>
         <Container>
             <h1>Wilders Book</h1>
         </Container>
       </Header>
       <MainContainer>
-            <PageTitle>Wilders</PageTitle>
-            <section className="card-row">
-              {wildersStudent.map((wilder) => (
-                <Wilder
-                  key={wilder.id}
-                  firstname={wilder.firstname}
-                  lastname={wilder.lastname}
-                  skills={wilder.skills}
-                  description={wilder.description}
-                />
-              ))}
-        </section>
+        <Home />
       </MainContainer>
       <Footer>
         <Container>
           <Paragraph>&copy; 2022 Wild Code School</Paragraph>
         </Container>
       </Footer>
-    </div>
+    </>
   );
 }
 
---
+++ frontend-webapp/src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+import { SectionTitle, CardRow } from "./Home.styled";
+
+import Wilder from "../components/Wilder";
+
+import wilders from "../data/wilders";
+
+const Home = () => {
+    const [wildersStudent] = useState(wilders);
+
+    return (
+        <>
+            <SectionTitle>Wilders</SectionTitle>
+            <CardRow>
+              {wildersStudent.map((wilder) => (
+                <Wilder
+                  key={wilder.id}
+                  firstname={wilder.firstname}
+                  lastname={wilder.lastname}
+                  skills={wilder.skills}
+                  description={wilder.description}
+                />
+              ))}
+        </CardRow>
+        </>
+    );
+};
+
+export default Home;
