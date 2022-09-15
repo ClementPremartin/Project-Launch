7311cad54b1fcb24c1796c8864152b006889d6e6
ClementPremartin <clementpremartin@gmail.com>
2022-09-15 15:46:30 +0200

    fetch the db to add wilders

--- frontend-webapp/src/pages/Home.jsx
+++ frontend-webapp/src/pages/Home.jsx
@@ -1,19 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 import { SectionTitle, CardRow } from "./Home.styled";
 
 import Wilder from "../components/Wilder/Wilder";
 
-import wilders from "../data/wilders";
-
 const Home = () => {
-    const [wildersStudent] = useState(wilders);
+    const [wildersStudent, setWildersStudent] = useState([]);
+
+    useEffect(() => {
+      axios
+        .get('/wilders')
+          .then((res) => {
+            setWildersStudent(res.data)
+          }).catch((err) => {
+            console.log(err);
+          })
+      // (async () => {
+      //   try {
+      //         const response = await axios.get('/wilder')
+      //         setWildersStudent(response.data);
+      //   } catch (error) {
+      //         console.log(error.response.body);
+      //   }
+      // })
+    }, []);
 
     return (
         <>
+        {console.log(wildersStudent)}
             <SectionTitle>Wilders</SectionTitle>
             <CardRow>
-              {wildersStudent.map((wilder) => (
+              {wildersStudent && wildersStudent.map((wilder) => (
                 <Wilder
                   key={wilder.id}
                   firstname={wilder.firstname}
