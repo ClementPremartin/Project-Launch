0f6911f032110ffb8ed8608d0860513a4aaad436
ClementPremartin <clementpremartin@gmail.com>
2022-09-15 11:14:09 +0200

    add readme file

---
+++ README.md
@@ -0,0 +1,31 @@
+# Work-study live coding 2022-09
+
+## Back-end (Node.js API)
+
+Install dependencies:
+
+```
+cd back-end
+npm install
+```
+
+Start server in development mode:
+
+```
+npm run start:watch
+```
+
+## Web app front-end (Create React App)
+
+Install dependencies:
+
+```
+cd web-app
+npm install
+```
+
+Start server in development mode:
+
+```
+npm run start
+```
--- frontend-webapp/src/data/wilders.js
+++ frontend-webapp/src/data/wilders.js
@@ -1,4 +1,5 @@
-const wilders = [{
+const wilders = [
+    {
     id: "6041673a-dbd8-409a-8269-f076ed2db967",
     firstname: "Philippe",
     lastname: "LeBrigant",
@@ -18,24 +19,52 @@ const wilders = [{
             skill_name: "JS",
             rate: 2
         }
-    ]
-},
-{
-    id: "b927a11c-b5f4-45e3-9dbb-7b283bda1154",
-    firstname: "Jeanjean",
-    lastname: "Bon",
-    description: "Je suis passionné de maquette en allumette et j'apprécie particulièrement lire des mangas.",
-    school: {
-        id: "d54ffaec-5d78-4539-b2b0-e7f05e7e7029",
-        city_name: "Brest"
+        ]
     },
-    skills: [
-        {
-            id: "9af758f9-2f01-4f3e-bbba-9bf0dec5ef60",
-            skill_name: "Java",
-            rate: 1
-        }
-    ]
-}];
+    {
+        id: "b927a11c-b5f4-45e3-9dbb-7b283bda1154",
+        firstname: "Jeanjean",
+        lastname: "Bon",
+        description: "Je suis passionné de maquette en allumette et j'apprécie particulièrement lire des mangas.",
+        school: {
+            id: "d54ffaec-5d78-4539-b2b0-e7f05e7e7029",
+            city_name: "Brest"
+        },
+        skills: [
+            {
+                id: "9af758f9-2f01-4f3e-bbba-9bf0dec5ef60",
+                skill_name: "Java",
+                rate: 1
+            }
+        ]
+    },
+    {
+        id: "b927a11c-b5f4-45e3-9dbb-7b283bda1154",
+        firstname: "Super",
+        lastname: "Prof",
+        description: "J'aime l'enseignement. Je suis passionné de maquette en allumette et j'apprécie particulièrement lire des mangas'.",
+        school: {
+            id: "d54ffaec-5d78-4539-b2b0-e7f05e7e7125",
+            city_name: "Brest"
+        },
+        skills: [
+            {
+                id: "9af758f9-2f01-4f3e-bbba-9bf0dec5ef60",
+                skill_name: "Java",
+                rate: 1
+            },
+            {
+                id: "9af758f9-2f01-4f3e-bbba-9bf0dec5ef60",
+                skill_name: "JS",
+                rate: 1
+            },
+            {
+                id: "9af758f9-2f01-4f3e-bbba-9bf0dec5ef60",
+                skill_name: "PHP",
+                rate: 1
+            }
+        ]
+    }
+];
 
 export default wilders;
