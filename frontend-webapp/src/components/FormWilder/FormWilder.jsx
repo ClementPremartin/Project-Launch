d222b010806940ff593356b92bf0d9745259f961
ClementPremartin <clementpremartin@gmail.com>
2022-09-16 14:42:20 +0200

    adding FormWilder styled file

--- frontend-webapp/src/components/FormWilder/FormWilder.jsx
+++ frontend-webapp/src/components/FormWilder/FormWilder.jsx
@@ -25,54 +25,56 @@ export default function App() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="firstname">
-        Prénom
-        <input
-          type="text"
-          placeholder="Jeanjean"
-          {...register("firstname", {
-            required: true,
-            minLength: 2,
-            maxLength: 35,
-          })}
-        />
-      </label>
-      <label htmlFor="lastname">
-        Nom
-        <input
-          type="text"
-          placeholder="Bon"
-          {...register("lastname", {
-            required: true,
-            minLength: 2,
-            maxLength: 35,
-          })}
-        />
-      </label>
-      <label htmlFor="city_name">
-        Campus
-        <select
-          name="city_name"
-          {...register("city_name")}
-          className={`form-control ${errors.city_name ? "is-invalid" : ""}`}
-        >
-          <option defaultValue="Lyon">Lyon</option>
-          <option value="Brest">Brest</option>
-        </select>
-      </label>
-      <label htmlFor="description">
-        Description
-        <input
-          type="text"
-          placeholder="Ecrivez votre description"
-          {...register("description", {
-            maxLength: 250,
-          })}
-        />
-      </label>
-      <input type="submit" />
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="firstname">
+          Prénom
+          <input
+            type="text"
+            placeholder="Jeanjean"
+            {...register("firstname", {
+              required: true,
+              minLength: 2,
+              maxLength: 35,
+            })}
+          />
+        </label>
+        <label htmlFor="lastname">
+          Nom
+          <input
+            type="text"
+            placeholder="Bon"
+            {...register("lastname", {
+              required: true,
+              minLength: 2,
+              maxLength: 35,
+            })}
+          />
+        </label>
+        <label htmlFor="city_name">
+          Campus
+          <select
+            name="city_name"
+            {...register("city_name")}
+            className={`form-control ${errors.city_name ? "is-invalid" : ""}`}
+          >
+            <option defaultValue="Lyon">Lyon</option>
+            <option value="Brest">Brest</option>
+          </select>
+        </label>
+        <label htmlFor="description">
+          Description
+          <input
+            type="text"
+            placeholder="Ecrivez votre description"
+            {...register("description", {
+              maxLength: 250,
+            })}
+          />
+        </label>
+        <input type="submit" />
+      </form>
+    </>
   );
 }
 
---
+++ frontend-webapp/src/components/FormWilder/FormWilder_styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.div`
+  &:first-child {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
