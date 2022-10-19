0050699e608191b5e8539b303efbd6226e466eac
ClementPremartin <clementpremartin@gmail.com>
2022-10-19 15:48:12 +0200

    setup getWilderById request in WilderResolver

--- backend/src/resolvers/WilderInput.ts
+++ backend/src/resolvers/WilderInput.ts
@@ -1,26 +1,32 @@
-import { MinLength } from "class-validator"
-import { ArgsType, Field } from "type-graphql";
+import { MinLength } from 'class-validator'
+import { ArgsType, Field } from 'type-graphql'
 
 @ArgsType()
 class AddWilderArgs {
-    @Field()
-    @MinLength(1, {
-        message: "Le prénom doit faire au minimum 1 caractère de long"
-    })
-    firstname: string;
+  @Field()
+  @MinLength(1, {
+    message: 'Le prénom doit faire au minimum 1 caractère de long',
+  })
+  firstname: string
 
-    @Field()
-    @MinLength(1, {message: "Le nom doit faire au minimum 1 caractère de long"})
-    lastname: string;
+  @Field()
+  @MinLength(1, { message: 'Le nom doit faire au minimum 1 caractère de long' })
+  lastname: string
 
-    @Field(() => String)
-    schoolId: string;
+  @Field(() => String)
+  schoolId: string
 
-    @Field(() => [String])
-    skills: string[];
+  @Field(() => [String])
+  skills: string[]
 
-    @Field()
-    description: string;
+  @Field()
+  description: string
 }
 
-export { AddWilderArgs }
+@ArgsType()
+class GetWilderByIdArgs {
+  @Field()
+  userId: string
+}
+
+export { AddWilderArgs, GetWilderByIdArgs }
--- backend/src/resolvers/WilderResolver.ts
+++ backend/src/resolvers/WilderResolver.ts
@@ -1,22 +1,35 @@
-import { Args, Mutation, Query, Resolver } from "type-graphql";
+import { Args, Mutation, Query, Resolver } from 'type-graphql'
 
-import Wilder from "../models/Wilder/WilderEntity";
-import WilderRepository from "../models/Wilder/WilderRepository";
-import { AddWilderArgs } from "./WilderInput";
+import Wilder from '../models/Wilder/WilderEntity'
+import WilderRepository from '../models/Wilder/WilderRepository'
+import { AddWilderArgs, GetWilderByIdArgs } from './WilderInput'
 
 @Resolver(Wilder)
 export default class WilderResolver {
+  @Query(() => [Wilder])
+  wilders(): Promise<Wilder[]> {
+    return WilderRepository.getWilders()
+  }
 
-    @Query(()=> [Wilder])
-    wilders(): Promise<Wilder[]> {
-        return WilderRepository.getWilders();
-    }
+  @Query(() => Wilder)
+  getWilderById(
+    @Args()
+    { userId }: GetWilderByIdArgs,
+  ): Promise<Wilder | null> {
+    return WilderRepository.getWilderById(userId)
+  }
 
-    @Mutation(() => Wilder)
-    addWilder(
-        @Args() {firstname, lastname, schoolId, skills, description}: AddWilderArgs
-    ): Promise<Wilder> {
-        return WilderRepository.createWilder(firstname, lastname, schoolId, skills, description)
-    }
-
+  @Mutation(() => Wilder)
+  addWilder(
+    @Args()
+    { firstname, lastname, schoolId, skills, description }: AddWilderArgs,
+  ): Promise<Wilder> {
+    return WilderRepository.createWilder(
+      firstname,
+      lastname,
+      schoolId,
+      skills,
+      description,
+    )
+  }
 }
