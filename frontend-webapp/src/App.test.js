d36f57178bf9bd2c9517abcaa4a711a06531ba94
ClementPremartin <clementpremartin@gmail.com>
2022-12-20 12:05:21 +0100

    setup front testing environment

--- frontend-webapp/src/App.test.js
+++ frontend-webapp/src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from "./App";
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('renders links', () => {
+  const client = new ApolloClient({
+  uri: '/',
+  cache: new InMemoryCache(),
+})
+  it('render nouveau wilder link', () => {
+    render(
+    <ApolloProvider client={client}>
+      <Router>
+        <App />
+      </Router>
+    </ApolloProvider>
+    );
+    const linkElement = screen.getByText(/Nouveau Wilder/i)
+    expect(linkElement).toBeInTheDocument()
+  })
 });
